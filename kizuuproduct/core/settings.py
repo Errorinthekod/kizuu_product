245b89fc2ed0cd6889600c684bddf3b0fea3af9e
Errorinthekod <ashataskerbekov6@gmail.com>
2026-02-20 21:27:25 +0600

    fixed rest_framework in settings.py/INSTALLED_APPS

--- kizuuproduct/core/settings.py
+++ kizuuproduct/core/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Packages
-    'djangorestframework',
+    'rest_framework',
     'rest_framework.authtoken',
     'django-extensions',
 ]
