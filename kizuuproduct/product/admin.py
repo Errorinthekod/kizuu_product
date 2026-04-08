946c30493fbe6782b2c16fb88a98e4ac3dfe17ce
Errorinthekod <ashataskerbekov6@gmail.com>
2026-04-09 00:05:21 +0600

    update product/admin.py

--- kizuuproduct/product/admin.py
+++ kizuuproduct/product/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, User
 
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username',
+                    'email',
+                    'date_joined',
+                    'last_login'
+                    )
+
+    list_fiter = (
+                    'is_staff',
+                    'is_active',
+                    'is_superuser'
+    )
+
+    search_fields = ('username', 'email')
+    ordering = ('username', 'email')
 
 
 @admin.register(Category)
--- kizuuproduct/product/models.py
+++ kizuuproduct/product/models.py
@@ -31,6 +31,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
     EMAIL_FIELD = 'email'
+
     objects = UserManager()
     def __str__(self):
         return f"{self.email} {self.username}"
